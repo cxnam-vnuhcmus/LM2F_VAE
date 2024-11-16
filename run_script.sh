bfefb22ad2a57ad70d623e98799ad3466a11bb36
cxnam <cxnam@fit.hcmus.edu.vn>
2024-11-16 09:53:30 +0000

    commit

---
+++ run_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+### Comment lines start with ## or #+space
+### Slurm option lines start with #SBATCH
+
+### Here are the SBATCH parameters that you should always consider:
+##SBATCH --time=0-00:05:00   ## days-hours:minutes:seconds
+#SBATCH --mem 6000M         ## 3GB ram (hardware ratio is < 4GB/core)
+#SBATCH --ntasks=1          ## Not strictly necessary because default is 1
+#SBATCH --cpus-per-task=10   ## Use greater than 1 for parallelized jobs
+#SBATCH --gpus=1
+
+### Here are other SBATCH parameters that you may benefit from using, currently commented out:
+#SBATCH --job-name=LM2F ## job name
+#SBATCH --output=job.out  ## standard out file
+
+# source activate myenv
+
+python main.py \
+  --config config/model-lm-vae.json \
+  --data_root /media/cxnam/MEAD \
+  --data_file /media/cxnam/LM2F_VAE/assets/datas_norm/M003.txt \
+  --suffix M003_lm_vae_v42 \
+  --model model_lm_vae.model_v4_2.Model \
+  --dataset model_lm_vae.dataset_v7.Dataset \
+  --log_samples /media/cxnam/LM2F_VAE/assets/samples/M003/samples_lm_vae_v42 \
+  --skip-train-val \
+  --epochs 300
+
+echo 'finished'
