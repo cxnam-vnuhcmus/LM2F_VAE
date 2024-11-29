4ed37f6fcd71c795c6a8e5a48d516cfe0f11dec3
Hoai-Thuong Dang <dangthuongtg05@gmail.com>
2024-11-29 20:20:15 +0700

    commit

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 #Train
 python main.py \
-  --config config/model-lm-vae.json \
-  --data_root /home/cxnam/Documents/MEAD \
+  --config config/model-lm-vae-gan.json \
+  --data_root /media/cxnam/NewVolume/MEAD \
   --data_file ./assets/datas_norm/M003.txt \
-  --suffix M003_lm_vae_v7 \
-  --model model_lm_vae.model_v7.Model \
+  --suffix M003_lm_vae_v9 \
+  --model model_lm_vae.model_v9.Model \
   --dataset model_lm_vae.dataset_v8.Dataset \
-  --log_samples /home/cxnam/Documents/MyWorkingSpace/LM2F_VAE/assets/samples/M003/samples_lm_vae_v7 \
+  --log_samples /home/cxnam/Documents/MyWorkingSpace/LM2F_VAE/assets/samples/M003/samples_lm_vae_v9 \
   --skip-train-val \
   --epochs 300 \
-  --pretrained /home/cxnam/Documents/MyWorkingSpace/LM2F_VAE/assets/checkpoints/checkpoint_M003_lm_vae_v7_checkpoint_200.pt \
+  --pretrained /home/cxnam/Documents/MyWorkingSpace/LM2F_VAE/assets/checkpoints/checkpoint_M003_lm_vae_v9_checkpoint_200.pt \
   --n_folders 5 \
 
 #Train GAN
